82a3a572fcab4e7deae47f40bbd5e4ad814ef6e2
greg-sim58 <greg@sim58dev.com>
2021-07-26 18:11:32 +0200

    After first build

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,9 @@
+<template>
+    <ion-page>
+        <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>List A</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    </ion-page>
+</template>
